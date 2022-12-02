4d90a6a0edc3a405300f35f084168ac108534b86
Oleksandr Pysariev <apalexlife@gmail.com>
2022-12-02 12:50:35 +0200

    -- init commit;

---
+++ ngrok_connection/connection/base.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from ngrok_connection.exceptions import DataNotLoadedError, TunnelNotFoundError
+
+
+class BaseNgrokConnection(ABC):
+
+    def __init__(
+            self,
+            ngrok_host: str = "http://localhost",
+            ngrok_port: int = 4040
+    ):
+        self.ngrok_host = ngrok_host
+        self.ngrok_port = ngrok_port
+        self.data: dict[str, list[dict] | Any] | None = None
+
+    @property
+    def ngrok_url(self):
+        return f"{self.ngrok_host}:{self.ngrok_port}/api/tunnels"
+
+    @abstractmethod
+    def load_tunnel_data(self) -> None:
+        raise NotImplementedError
+
+    @property
+    def tunnels(self) -> list[dict]:
+        if not self.data:
+            raise DataNotLoadedError()
+
+        return self.data["tunnels"]
+
+    def get_tunnel_url(self, tunnel_index: int = 0) -> str:
+        tunnels = self.tunnels
+        if tunnel_index >= len(tunnels):
+            raise TunnelNotFoundError(tunnel_index)
+
+        return tunnels[tunnel_index]["public_url"]
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ngrok_connection',
+    version='1.0.0',
+    author='Oleksandr Pysariev',
+    author_email='apalexlife@gmail.com',
+    packages=find_packages(),
+    description='Util to get ngrok url',
+    extras_require={
+        "sync": ["requests"],
+        "async": ["aiohttp"],
+    }
+)
